b517020d4a900396ce2bddfc346636a3990483eb
Kerem Celik <kerem.celik@pfizer.com>
2022-01-26 17:07:35 -0500

    remove requirmetns

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
-
 setup(
     name="my-py-package",
     version="0.0.1",
@@ -15,7 +13,6 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/keremcelik/my-py-package",
     packages=find_packages(),
-    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.7",
     ],
